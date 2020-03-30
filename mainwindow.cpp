692180aa2e07c878b9bd6985321743f6405ecc64
Matt Moore <matt@mattmoore.io>
2020-03-30 09:17:39 -0500

    Qt screenshot

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,6 +6,8 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    connect(ui->screenshotButton, SIGNAL(released()), this, SLOT(screenshot()));
 }
 
 MainWindow::~MainWindow()
@@ -13,3 +15,12 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+void MainWindow::screenshot() {
+    QScreen *screen = QGuiApplication::primaryScreen();
+
+    if (const QWindow *window = windowHandle())
+        screen = window->screen();
+    if (!screen)
+        return;
+    screen->grabWindow(0);
+}
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,8 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QWindow>
+#include <QScreen>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -14,6 +16,9 @@ class MainWindow : public QMainWindow
 public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
+
+public slots:
+    void screenshot();
 
 private:
     Ui::MainWindow *ui;
